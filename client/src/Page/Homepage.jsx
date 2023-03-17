d97d250a924cb52c6236ffa6fa862c97b8e305cc
banksss33 <super.b99@hotmail.com>
2023-03-17 21:10:35 +0700

    Modify carousel to rounded and change to crossfade

--- client/src/Page/Homepage.jsx
+++ client/src/Page/Homepage.jsx
@@ -5,19 +5,22 @@ const newItem = [
   //New Movie data that pass to carousel
   {
     id: 1,
-    image: "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/C8FB90AA53A8A57DE288B1FA92E740DC1BD8003D8898E3C2557413129FD4822D/scale?width=1200&aspectRatio=1.78&format=jpeg",
+    image:
+      "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/C8FB90AA53A8A57DE288B1FA92E740DC1BD8003D8898E3C2557413129FD4822D/scale?width=1200&aspectRatio=1.78&format=jpeg",
     chead: "Ant-Man and the Wasp: Quantumania",
     desc: "Ant-Man and the Wasp find themselves exploring the Quantum , interacting with strange new creatures and embarking on an adventure that pushes them beyond the limits of what they thought was possible.",
   },
   {
     id: 2,
-    image: "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/C8FB90AA53A8A57DE288B1FA92E740DC1BD8003D8898E3C2557413129FD4822D/scale?width=1200&aspectRatio=1.78&format=jpeg",
+    image:
+      "https://markpeak.net/wp-content/uploads/2022/03/shawshank.jpg",
     chead: "Ant-Man and the Wasp: Quantumania",
     desc: "Ant-Man and the Wasp find themselves exploring the Quantum , interacting with strange new creatures and embarking on an adventure that pushes them beyond the limits of what they thought was possible.",
   },
   {
     id: 3,
-    image: "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/C8FB90AA53A8A57DE288B1FA92E740DC1BD8003D8898E3C2557413129FD4822D/scale?width=1200&aspectRatio=1.78&format=jpeg",
+    image:
+      "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/C8FB90AA53A8A57DE288B1FA92E740DC1BD8003D8898E3C2557413129FD4822D/scale?width=1200&aspectRatio=1.78&format=jpeg",
     chead: "Ant-Man and the Wasp: Quantumania",
     desc: "Ant-Man and the Wasp find themselves exploring the Quantum , interacting with strange new creatures and embarking on an adventure that pushes them beyond the limits of what they thought was possible.",
   },
@@ -27,19 +30,22 @@ const popuItem = [
   //Popular Movie data that pass to carousel
   {
     id: 1,
-    image: "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/C8FB90AA53A8A57DE288B1FA92E740DC1BD8003D8898E3C2557413129FD4822D/scale?width=1200&aspectRatio=1.78&format=jpeg",
+    image:
+      "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/C8FB90AA53A8A57DE288B1FA92E740DC1BD8003D8898E3C2557413129FD4822D/scale?width=1200&aspectRatio=1.78&format=jpeg",
     chead: "Ant-Man and the Wasp: Quantumania",
     desc: "Ant-Man and the Wasp find themselves exploring the Quantum , interacting with strange new creatures and embarking on an adventure that pushes them beyond the limits of what they thought was possible.",
   },
   {
     id: 2,
-    image: "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/C8FB90AA53A8A57DE288B1FA92E740DC1BD8003D8898E3C2557413129FD4822D/scale?width=1200&aspectRatio=1.78&format=jpeg",
+    image:
+      "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/C8FB90AA53A8A57DE288B1FA92E740DC1BD8003D8898E3C2557413129FD4822D/scale?width=1200&aspectRatio=1.78&format=jpeg",
     chead: "Ant-Man and the Wasp: Quantumania",
     desc: "Ant-Man and the Wasp find themselves exploring the Quantum , interacting with strange new creatures and embarking on an adventure that pushes them beyond the limits of what they thought was possible.",
   },
   {
     id: 3,
-    image: "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/C8FB90AA53A8A57DE288B1FA92E740DC1BD8003D8898E3C2557413129FD4822D/scale?width=1200&aspectRatio=1.78&format=jpeg",
+    image:
+      "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/C8FB90AA53A8A57DE288B1FA92E740DC1BD8003D8898E3C2557413129FD4822D/scale?width=1200&aspectRatio=1.78&format=jpeg",
     chead: "Ant-Man and the Wasp: Quantumania",
     desc: "Ant-Man and the Wasp find themselves exploring the Quantum , interacting with strange new creatures and embarking on an adventure that pushes them beyond the limits of what they thought was possible.",
   },
@@ -68,11 +74,11 @@ function Hub() {
           <Container>
             {" "}
             {/* Container3 */}
-            <Carousel>
+            <Carousel fade>
               {newItem.map((MItems) => (
                 <Carousel.Item key={MItems.id}>
                   <img
-                    className="d-block w-100"
+                    className="d-block w-100 rounded-5"
                     src={MItems.image}
                     alt="Data not found!"
                   />
@@ -95,11 +101,11 @@ function Hub() {
           <Container>
             {" "}
             {/* Container3 */}
-            <Carousel>
+            <Carousel fade>
               {popuItem.map((popuItems) => (
                 <Carousel.Item key={popuItems.id}>
                   <img
-                    className="d-block w-100"
+                    className="d-block w-100 rounded-5"
                     src={popuItems.image}
                     alt="Data not found!"
                   />
