b605600b5a1cc739ac3fb9685a099adf5c254eab
Nitikorn thanasutr <nitikorn@gmail.com>
2023-03-17 18:12:42 +0700

    add server and install axios in client

---
+++ server/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("haha");
+})
+
+app.listen(5000, () => {
+    console.log('server running on port 5000')
+})
